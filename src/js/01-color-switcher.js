f940df0695d2e9b7619ea0d497d79e4d405a82b8
Kuzmich87 <d.kuzmich87@gmail.com>
2022-09-07 14:29:56 +0300

    first is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
 
+const refs={
+   startBtn: document.querySelector('[data-start]'),
+   stopBtn: document.querySelector('[data-stop]'),
+}
+// console.log(refs.startBtn);
+// console.log(refs.stopBtn);
+
+let timerId = null;
+
+//* функция изминения цветов
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+//*Функция на кнопку старт
+refs.startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        color = getRandomHexColor();
+        document.body.setAttribute(
+        'style',
+        'background-color:'+ color,
+        );
+        refs.startBtn.setAttribute('disabled', true);
+    console.log(setInterval);
+  }, 1000);
+});
+
+//*Функция на кнопку стоп
+refs.stopBtn.addEventListener("click", () => {
+    refs.startBtn.removeAttribute('disabled');
+    clearInterval(timerId);
+});
+
+
+
+
+
+
+
