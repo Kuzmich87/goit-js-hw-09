971a0429929232eaee4b026dbca5ecfe093f0ebc
Kuzmich87 <d.kuzmich87@gmail.com>
2022-09-10 17:26:13 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,6 +9,38 @@ const inputField = document.querySelector('#datetime-picker');
 let timerId = null;
 let timeEnd = null;
 
+//*Принемает число, приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+//*-Принемает время в милисекундах
+//*-Высчитывает сколько в них вмещаеться часох/минут/секунд
+//*-Возвращает обьект с свойствами days, hours, minutes,seconds.
+//*-Адская копипаста со стека
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+ 
+  // Remaining seconds
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  
+
+  return { days, hours, minutes, seconds };
+}
+
 
 const options = {
   enableTime: true,
@@ -39,51 +71,25 @@ const timer = {
       const deltaTime = timeEnd - currentTime;
       const { days, hours, minutes, seconds } = convertMs(deltaTime);
       console.log(`${days}::${hours}::${minutes}::${seconds}`);
+
+      document.querySelector('[data-days]').innerHTML = days;
+      document.querySelector('[data-hours]').innerHTML = hours;
+      document.querySelector('[data-minutes]').innerHTML = minutes;
+      document.querySelector('[data-seconds]').innerHTML = seconds;
+      
+      if (deltaTime <= 0) {
+      clearInterval(timerId);
+      } 
     }, 1000
     )
-    if (this.deltaTime < 0) {
-      this.stopTimer();
-    }
+   
   }
    
 };
-function stopTimer(){
-    clearInterval(this.timerId);
-  }
 
-//*Принемает число, приводит к строке и добавляет в начало 0, если число меньше 2-х знаков
-function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-}
-
-//*-Принемает время в милисекундах
-//*-Высчитывает сколько в них вмещаеться часох/минут/секунд
-//*-Возвращает обьект с свойствами days, hours, minutes,seconds.
-//*-Адская копипаста со стека
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = addLeadingZero(Math.floor(ms / day));
-  document.querySelector('[data-days]').innerHTML = days;
-  // Remaining hours
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  document.querySelector('[data-hours]').innerHTML = hours;
-  // Remaining minutes
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  document.querySelector('[data-minutes]').innerHTML = minutes;
-  // Remaining seconds
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-  document.querySelector('[data-seconds]').innerHTML = seconds;
-
-  return { days, hours, minutes, seconds };
-}
-
-btnStart.addEventListener('click',timer.start())
+btnStart.addEventListener("click", () => {
+  timer.start();
+})
 
 //* Инициализация библиотеки
  flatpickr(document.querySelector('input[type="text"]'), options)
